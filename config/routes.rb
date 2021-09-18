baed1b2ce2b8dd52b875a16988226434e364b171
paulo-castellan <paulo.castellan@gmail.com>
2021-09-18 14:43:51 -0300

    antes de começar layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  namespace :site do
+    get 'welcome/index'
+  end
+  namespace :users_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+
+  devise_for :users
+  devise_for :admins
+
+  
+  get 'inicio', to: 'site/welcome#index'
+  
+  root to: 'site/welcome#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
